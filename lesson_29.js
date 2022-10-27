b28950aba261fe6feccf4a277ff59108cbd7c995
Константин Ходаков <hodakov.k@gmail.com>
2022-10-27 22:33:35 +0300

    new file: lesson_29

---
+++ lesson_29.js
@@ -0,0 +1,6 @@
+const firstName = 'Joffrey';
+const greeting = 'Hello';
+
+// Обратите внимание на ограничители строки, это бектики
+// Интерполяция не работает с одинарными и двойными кавычками
+console.log(`${greeting}, ${firstName}!`);
