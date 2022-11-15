ee11511ff14784c1b51b7297cb65e41f7c8e8140
Константин Ходаков <hodakov.k@gmail.com>
2022-11-15 23:16:08 +0300

    edit file: codewears.js

--- codewears.js
+++ codewears.js
@@ -1,21 +1,8 @@
-let n = 89;
-var p = 1;
-var result = 0;
-var number = 0;
-var count = n.length - 1;
-for (var i = 0; i < n.length; i++) {
-    result = result + n[i] ** p;
-    p++;
-}
-for (var i = 0; i < n.length; i++){
-    number = number + n[i] * (10 ** (count));
-    count--;
-}
-if(result % number === 0){
-    console.log(result / number);
-    return result / number;
-}
-else{
-    console.log(-1)
-    return -1;
+let text = "Hey fellow warriors";
+let words = [];
+words = text.split(" ");
+for (let index = 0; index < words.length; index++) {
+    if(words[index].length >= 5){
+        
+    }
 }
