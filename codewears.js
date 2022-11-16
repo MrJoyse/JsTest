7bb1d3e58c6e4c6ade497c5bdf4d47b33e1f7bf3
Константин Ходаков <hodakov.k@gmail.com>
2022-11-16 18:49:24 +0300

    status: done

--- codewears.js
+++ codewears.js
@@ -1,8 +1,17 @@
 let text = "Hey fellow warriors";
+let result = "";
 let words = [];
+let word = [];
 words = text.split(" ");
 for (let index = 0; index < words.length; index++) {
     if(words[index].length >= 5){
-        
+        word = words[index].split("").reverse();
+        result = result + word.join('') + " ";
+    }
+    else{
+        word = words[index].split("");
+        result = result + word.join('') + " " ;
     }
 }
+result = result.trim();
+console.log(result);
